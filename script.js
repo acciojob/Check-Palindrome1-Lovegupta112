fe0e6bf22255fd6d81e87276e00139406b79b247
Love Gupta <90507983+Lovegupta112@users.noreply.github.com>
2023-05-15 19:49:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-	str=str.toLowerCase();
-	let arr=str.split(" ");
-    let s="";
-	 arr.forEach((str)=>{
-		  if(str!=''){
-           s=s+str;
-		  }
-	 })
+
+	// let arr=str.split(" ");
+ //    let s="";
+	//  arr.forEach((str)=>{
+	// 	  if(str!=''){
+ //           s=s+str;
+	// 	  }
+	//  })
+		s=str.toLowerCase();
 let start=0;
 let end=s.length-1;
 	while(start<end){
